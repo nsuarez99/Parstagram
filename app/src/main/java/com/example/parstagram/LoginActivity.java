9da4b22510549f77cb96bef10d87bdf35309dcbb
nsuarez99 <ngsuarez99@gmail.com>
2020-07-07 16:37:50 -0400

    login page and app icon

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -3,12 +3,32 @@ package com.example.parstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    EditText loginUsername;
+    EditText loginPassword;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginUsername = findViewById(R.id.loginUsername);
+        loginPassword = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
+    }
+
+    public void loginClick(View view){
+        Log.i(TAG, "login button clicked, attemtping to login");
+        String username = loginUsername.getText().toString();
+        String password = loginPassword.getText().toString();
+
     }
 }
