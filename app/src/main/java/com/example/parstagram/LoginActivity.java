f6105a155f3b5ee35fc002d0dba1d23b97e62aac
nsuarez99 <ngsuarez99@gmail.com>
2020-07-07 20:50:08 -0400

    connected login page to parse backend so user can login and go to mainactivity

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -2,11 +2,17 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,12 +29,36 @@ public class LoginActivity extends AppCompatActivity {
         loginUsername = findViewById(R.id.loginUsername);
         loginPassword = findViewById(R.id.loginPassword);
         loginButton = findViewById(R.id.loginButton);
+
+        if (ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
     }
 
     public void loginClick(View view){
         Log.i(TAG, "login button clicked, attemtping to login");
         String username = loginUsername.getText().toString();
         String password = loginPassword.getText().toString();
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null){
+                    //TODO better error handling
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(LoginActivity.this, "Issue with Login", Toast.LENGTH_SHORT);
 
+                }
+                else{
+                    goMainActivity();
+                    Toast.makeText(LoginActivity.this, "Sucess", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
