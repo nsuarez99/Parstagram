21372905ce63089ed5d65ba95163f2f9d9a935f2
nsuarez99 <ngsuarez99@gmail.com>
2020-07-07 22:03:44 -0400

    added toolbars to layouts and created compose layout

--- app/src/main/java/com/example/parstagram/ComposeActivity.java
+++ app/src/main/java/com/example/parstagram/ComposeActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    public static final String TAG = "MainActivity"; 
+public class ComposeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_compose);
     }
 }
