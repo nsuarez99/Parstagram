2caba1a28d59851b1486bc0a889572179861eb5d
nsuarez99 <ngsuarez99@gmail.com>
2020-07-10 19:01:40 -0400

    added enless scroll on profile fragemnt

--- app/src/main/java/com/example/parstagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/parstagram/fragments/ProfileFragment.java
@@ -10,8 +10,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.parstagram.EndlessRecyclerViewScrollListener;
 import com.example.parstagram.Post;
 import com.example.parstagram.PostsAdapter;
 import com.example.parstagram.R;
@@ -27,7 +29,16 @@ public class ProfileFragment extends TimelineFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        scrollListener = new EndlessRecyclerViewScrollListener(gridLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                loadNextQueryPosts(posts.size());
+            }
+        };
+        recyclerView.addOnScrollListener(scrollListener);
     }
 
     @Override
@@ -38,7 +49,7 @@ public class ProfileFragment extends TimelineFragment {
         query.include(Post.KEY_USER);
         // limit query to latest 20 items
         query.setLimit(NUMBER_POSTS);
-        //only get posts by user
+        //only query this user
         query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
         // order posts by creation date (newest first)
         query.addDescendingOrder(Post.KEY_TIME);
@@ -51,7 +62,6 @@ public class ProfileFragment extends TimelineFragment {
                     Log.e(TAG, "Issue with getting posts", e);
                     return;
                 }
-
                 // save received posts to list and notify adapter of new data
                 adapter.clear();
                 adapter.addAll(objects);
